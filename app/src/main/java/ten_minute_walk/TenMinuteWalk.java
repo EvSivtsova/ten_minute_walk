74d58214db99c072a31cc75dd39ffe6ba07aff31
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-10 16:20:58 +0000

    Return false if the walk is too short - hardcoded

---
+++ app/src/main/java/ten_minute_walk/TenMinuteWalk.java
@@ -0,0 +1,11 @@
+package ten_minute_walk;
+
+public class TenMinuteWalk {
+    public Boolean assess( char[] walk_array) {
+        return false;
+    }
+
+    protected int getLength(char[] walk_array) {
+        return 1;
+    }
+}
