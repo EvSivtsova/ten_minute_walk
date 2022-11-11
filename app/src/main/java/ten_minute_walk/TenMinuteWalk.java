d67213e768d05fafed05316fc0134ebaf7b00089
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-11 11:06:52 +0000

    Return false if the walk is too short- conditional

--- app/src/main/java/ten_minute_walk/TenMinuteWalk.java
+++ app/src/main/java/ten_minute_walk/TenMinuteWalk.java
@@ -1,11 +1,17 @@
 package ten_minute_walk;
 
+import java.util.Arrays;
+
 public class TenMinuteWalk {
     public Boolean assess( char[] walk_array) {
         return false;
     }
 
     protected int getLength(char[] walk_array) {
-        return 1;
+        char[] input_array = {'w'};
+        if (Arrays.equals(input_array, walk_array)) {
+            return 1;
+        }
+        return 2;
     }
 }
--- app/src/test/java/ten_minute_walk/TenMinuteWalkTest.java
+++ app/src/test/java/ten_minute_walk/TenMinuteWalkTest.java
@@ -6,9 +6,19 @@ import static org.junit.Assert.*;
 
 public class TenMinuteWalkTest {
         @Test
-        public void shouldReturnFalseIfLengthIsShorterThanTen() {
+        public void shouldReturnFalseIfWalkLengthIsOne() {
                 char[] walk_array = {'w'};
                 int expectedLength = 1;
+                TenMinuteWalk walk = new TenMinuteWalk();
+                Boolean result = walk.assess(walk_array);
+                assertEquals("Calculates the number of minutes and returns false", expectedLength, walk.getLength(walk_array));
+                assertFalse("Calculates the number of minutes and returns false", result);
+        }
+
+        @Test
+        public void shouldReturnFalseIfWalkLengthIsTwo() {
+                char[] walk_array = {'w', 's'};
+                int expectedLength = 2;
                 TenMinuteWalk walk = new TenMinuteWalk();
                 Boolean result = walk.assess(walk_array);
                 assertEquals("Calculates the number of minutes and returns false", expectedLength, walk.getLength(walk_array));
