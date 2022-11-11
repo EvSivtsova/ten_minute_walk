c0532984e84624297deef4c2a9ccf127cf282159
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-11 11:29:15 +0000

    Return true if walk is acceptable - hardcoded

--- app/src/main/java/ten_minute_walk/TenMinuteWalk.java
+++ app/src/main/java/ten_minute_walk/TenMinuteWalk.java
@@ -1,13 +1,16 @@
 package ten_minute_walk;
 
-import java.util.Arrays;
+import org.jetbrains.annotations.NotNull;
 
 public class TenMinuteWalk {
     public Boolean assess( char[] walk_array) {
-        return false;
+        if (this.getLength(walk_array) < 10) {
+            return false;
+        }
+        return true;
     }
 
-    protected int getLength(char[] walk_array) {
+    protected int getLength(char @NotNull [] walk_array) {
         return walk_array.length;
     }
 }
--- app/src/test/java/ten_minute_walk/TenMinuteWalkTest.java
+++ app/src/test/java/ten_minute_walk/TenMinuteWalkTest.java
@@ -11,7 +11,7 @@ public class TenMinuteWalkTest {
                 int expectedLength = 1;
                 TenMinuteWalk walk = new TenMinuteWalk();
                 Boolean result = walk.assess(walk_array);
-                assertEquals("Calculates the number of minutes and returns false", expectedLength, walk.getLength(walk_array));
+                assertEquals("Evaluates the length of the array", expectedLength, walk.getLength(walk_array));
                 assertFalse("Calculates the number of minutes and returns false", result);
         }
 
@@ -21,7 +21,7 @@ public class TenMinuteWalkTest {
                 int expectedLength = 2;
                 TenMinuteWalk walk = new TenMinuteWalk();
                 Boolean result = walk.assess(walk_array);
-                assertEquals("Calculates the number of minutes and returns false", expectedLength, walk.getLength(walk_array));
+                assertEquals("Evaluates the length of the array", expectedLength, walk.getLength(walk_array));
                 assertFalse("Calculates the number of minutes and returns false", result);
         }
 
@@ -31,7 +31,17 @@ public class TenMinuteWalkTest {
                 int expectedLength = 3;
                 TenMinuteWalk walk = new TenMinuteWalk();
                 Boolean result = walk.assess(walk_array);
-                assertEquals("Calculates the number of minutes and returns false", expectedLength, walk.getLength(walk_array));
+                assertEquals("Evaluates the length of the array", expectedLength, walk.getLength(walk_array));
                 assertFalse("Calculates the number of minutes and returns false", result);
+        }
+
+        @Test
+        public void shouldReturnTrueForAcceptableWalk() {
+                char[] walk_array = {'w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'};
+                int expectedLength = 10;
+                TenMinuteWalk walk = new TenMinuteWalk();
+                Boolean result = walk.assess(walk_array);
+                assertEquals("Evaluates the length of the array", expectedLength, walk.getLength(walk_array));
+                assertTrue ("Calculates the number of minutes and returns true", result);
         }
 }
